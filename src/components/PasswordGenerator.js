32651575be217af27638213b30a62e459e100b00
JoHoop <jolienhoop@gmail.com>
2021-10-04 18:29:28 +0200

    Separate wallpaper components

--- src/components/PasswordGenerator.js
+++ src/components/PasswordGenerator.js
@@ -1,70 +1,22 @@
 import React from 'react';
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import { Wallpaper, Widget } from './Wallpaper';
 
-const WallPaper = styled('div')({
-  position: 'absolute',
-  width: '100%',
-  height: '100%',
-  top: 0,
-  left: 0,
-  overflow: 'hidden',
-  zIndex: -1,
-  background: 'linear-gradient(rgb(255, 38, 142) 0%, rgb(255, 105, 79) 100%)',
-  transition: 'all 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s',
-  '&:before': {
-    content: '""',
-    width: '140%',
-    height: '140%',
-    position: 'absolute',
-    top: '-40%',
-    right: '-50%',
-    background:
-      'radial-gradient(at center center, rgb(62, 79, 249) 0%, rgba(62, 79, 249, 0) 64%)',
-  },
-  '&:after': {
-    content: '""',
-    width: '140%',
-    height: '140%',
-    position: 'absolute',
-    bottom: '-50%',
-    left: '-30%',
-    background:
-      'radial-gradient(at center center, rgb(247, 237, 225) 0%, rgba(247, 237, 225, 0) 70%)',
-    transform: 'rotate(30deg)',
-  },
-});
-
-const Widget = styled('div')(({ theme }) => ({
-  padding: 16,
-  borderRadius: 16,
-  width: 343,
-  maxWidth: '100%',
-  margin: 'auto',
-  position: 'relative',
-  zIndex: 1,
-  backgroundColor:
-    theme.palette.mode === 'dark' ? 'rgba(0,0,0,0.6)' : 'rgba(255,255,255,0.4)',
-  backdropFilter: 'blur(40px)',
-}));
-
-const generatePassword = () => {
+export const PasswordGenerator = () => {
   const chars =
     '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  const length = 6;
-  return Array(length)
+  const length = 18;
+  const blockSize = 6;
+  const delimiter = '-';
+  const password = Array(length)
     .fill(chars)
     .map(function (c) {
       return c[Math.floor(Math.random() * c.length)];
     })
-    .join('');
-};
-
-export const PasswordGenerator = () => {
-  const parts = [generatePassword(), generatePassword(), generatePassword()];
-  const password = parts.join('-');
+    .join('')
+    .match(new RegExp('.{1,' + blockSize + '}', 'g'))
+    .join(delimiter);
 
   return (
     <Box sx={{ width: '100%', overflow: 'hidden' }}>
@@ -78,7 +30,7 @@ export const PasswordGenerator = () => {
           <Typography>{password}</Typography>
         </Box>
       </Widget>
-      <WallPaper />
+      <Wallpaper />
     </Box>
   );
 };
---
+++ src/components/Wallpaper.js
@@ -0,0 +1,47 @@
+import { styled } from '@mui/material/styles';
+
+export const Wallpaper = styled('div')({
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  top: 0,
+  left: 0,
+  overflow: 'hidden',
+  zIndex: -1,
+  background: 'linear-gradient(rgb(255, 38, 142) 0%, rgb(255, 105, 79) 100%)',
+  transition: 'all 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s',
+  '&:before': {
+    content: '""',
+    width: '140%',
+    height: '140%',
+    position: 'absolute',
+    top: '-40%',
+    right: '-50%',
+    background:
+      'radial-gradient(at center center, rgb(62, 79, 249) 0%, rgba(62, 79, 249, 0) 64%)',
+  },
+  '&:after': {
+    content: '""',
+    width: '140%',
+    height: '140%',
+    position: 'absolute',
+    bottom: '-50%',
+    left: '-30%',
+    background:
+      'radial-gradient(at center center, rgb(247, 237, 225) 0%, rgba(247, 237, 225, 0) 70%)',
+    transform: 'rotate(30deg)',
+  },
+});
+
+export const Widget = styled('div')(({ theme }) => ({
+  padding: 16,
+  borderRadius: 16,
+  width: 343,
+  maxWidth: '100%',
+  margin: 'auto',
+  position: 'relative',
+  zIndex: 1,
+  backgroundColor:
+    theme.palette.mode === 'dark' ? 'rgba(0,0,0,0.6)' : 'rgba(255,255,255,0.4)',
+  backdropFilter: 'blur(40px)',
+}));
